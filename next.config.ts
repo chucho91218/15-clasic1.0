53319bd81a20b79f81bd5bd3e59d2cd38f524f2b
chucho91218 <aguslriver@gmail.com>
2026-03-31 13:35:13 -0300

    Prepare static export for Cloudflare Pages

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,12 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: "export",
   reactCompiler: true,
   reactStrictMode: true,
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
